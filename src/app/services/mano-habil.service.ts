3f7558dbf60b52ccfa233c92f69eed8c5b805e4f
Ivan Naumann Thostrup <naumannthostrupivan@gmail.com>
2024-10-22 15:55:02 -0300

    update

--- src/app/services/mano-habil.service.ts
+++ src/app/services/mano-habil.service.ts
@@ -8,8 +8,20 @@ export class ManoHabilService {
   private manoHabilSubject = new BehaviorSubject<string>('hand-right');
   manoHabil$ = this.manoHabilSubject.asObservable();
 
+  constructor() {
+    // Cargar mano habil desde localStorage en la inicialización del servicio
+    const manoHabilFromStorage = localStorage.getItem('manoHabil');
+    if (manoHabilFromStorage) {
+      this.manoHabilSubject.next(manoHabilFromStorage);
+    }
+  }
+
   cambiarManoHabil() {
     const nuevaManoHabil = this.manoHabilSubject.value === 'hand-left' ? 'hand-right' : 'hand-left';
     this.manoHabilSubject.next(nuevaManoHabil);
+  }
+
+  setManoHabil(manoHabil: string) {
+    this.manoHabilSubject.next(manoHabil);
   }
 }
